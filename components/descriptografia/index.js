200883ee9893daff59d7f20ce7cdc1602646bbc7
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2023-10-17 12:49:36 -0300

    17/10

---
+++ components/descriptografia/index.js
@@ -0,0 +1,3 @@
+export default function descriptografar(){
+  console.log(semMensagem)
+}
--- telaprincipal/index.js
+++ telaprincipal/index.js
@@ -1,4 +1,9 @@
 import criptografar from '/components/criptografia/index.js'
+import descriptografar from '/components/descriptografia/index.js'
 
 const criptografador = document.querySelector('.criptografar');
 criptografador.addEventListener('click', criptografar);
+
+const descriptografador = document.querySelector('.criptografar');
+descriptografador.addEventListener('click', descriptografar);
+
